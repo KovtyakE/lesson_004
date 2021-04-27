59f93a9fa629a5c8b2b37f65f41247604632212c
EVG <evgeniy-kovtyak@mail.ru>
2021-04-27 12:19:03 +0300

    comments for completed task 2

--- 02_global_color.py
+++ 02_global_color.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+# painting library
 import simple_draw as sd
 
 sd.set_screen_size(1500, 850)
-
+#first point needed to start drawing
 first_point = sd.get_point(150, 150)
+# defining var for some colors
 red = sd.COLOR_RED
 orange = sd.COLOR_ORANGE
 yellow = sd.COLOR_YELLOW
@@ -12,6 +14,7 @@ cyan = sd.COLOR_CYAN
 blue = sd.COLOR_BLUE
 purple = sd.COLOR_PURPLE
 
+#creating dictionary for all colors
 color_dict = {
     1: red,
     2: orange,
@@ -34,9 +37,11 @@ color_dict = {
 # и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
-
+#function for drawing, which uses info about count of angles, angle width, start coordinates, length of line(vector)
+#and it's color
 def drawing(iterations, angle, start_point, length, color):
     into_start_point = start_point
+# new var for cycle changing, iterations -1 because last line we drawing with two points
     for _ in range(iterations - 1):
         every_angle = int(360 / iterations)
         vector = sd.get_vector(start_point=into_start_point, angle=angle, length=length, width=1)
@@ -46,7 +51,7 @@ def drawing(iterations, angle, start_point, length, color):
         sd.sleep(0.1)
     sd.line(start_point=into_start_point, end_point=start_point, color=color, width=1)
 
-
+#function for asking user about color. every iteration for new figure changing start point
 def all_figures(start_point, angle, length):
     asked_color = 0
     while asked_color == 0:
